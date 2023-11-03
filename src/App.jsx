ce62e15442084f5d36c44ecea38c0f9bd1afee9a
mmakash <musaddekurakash@gmail.com>
2023-11-04 02:33:32 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import Home from './Pages/Home';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello App</h1>
-    </div>
+    <BrowserRouter>
+    <Home />
+    </BrowserRouter>
   );
 };
 
